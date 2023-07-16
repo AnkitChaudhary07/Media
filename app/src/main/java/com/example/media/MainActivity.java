6a1608bc034b4dba241f246a1492a8494b06dc79
Ankit <Ankichaudhary126@gmail.com>
2023-07-16 15:19:26 +0530

    Media app using remote source

---
+++ app/src/main/java/com/example/media/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.media;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    private Button play, pause;
+    SeekBar seekBar;
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        play = findViewById(R.id.play);
+        pause = findViewById(R.id.pause);
+        seekBar = findViewById(R.id.seekBar);
+
+        //Media Player using local source...
+       // mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.bones);
+
+        //Media Player using remote source...
+        mediaPlayer = new MediaPlayer();
+        try {
+//            mediaPlayer.setDataSource("https://djmaza.live/files/download/id/8231");  //UCL anthem...
+            mediaPlayer.setDataSource("https://djmaza.live/files/download/id/5730");  //On my way...
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                Toast.makeText(MainActivity.this, "Music is ready", Toast.LENGTH_SHORT).show();
+                seekBar.setMax(mediaPlayer.getDuration());
+                seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        if(fromUser) mediaPlayer.seekTo(progress);
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+
+                    }
+                });
+            }
+        });
+        mediaPlayer.prepareAsync();
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.start();
+            }
+        });
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.pause();
+            }
+        });
+    }
+}
